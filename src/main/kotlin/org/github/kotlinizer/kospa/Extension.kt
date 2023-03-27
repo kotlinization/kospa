0d78ff0b90b57446c5a0f1de50bab32d17585d2e
Milan Jovic <milanjovic92@live.com>
2023-03-28 00:54:49 +0200

    Removed unused classes.

--- src/main/kotlin/org/github/kotlinizer/kospa/Extension.kt
+++
@@ -1,9 +0,0 @@
-package org.github.kotlinizer.kospa
-
-import kotlin.reflect.KClass
-
-val <T : Page> KClass<T>.name
-    get() = simpleName ?: throw IllegalArgumentException("View must not be anonymous.")
-
-val <T : Fragment> KClass<T>.name
-    get() = simpleName ?: throw IllegalArgumentException("Fragment must not be anonymous.")
--- src/main/kotlin/org/github/kotlinizer/kospa/PageParams.kt
+++
@@ -1,8 +0,0 @@
-package org.github.kotlinizer.kospa
-
-import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.JsonNull
-
-data class PageParams(
-    val data: JsonElement = JsonNull
-)
